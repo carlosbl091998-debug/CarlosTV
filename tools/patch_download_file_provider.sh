14db18b70d34e15bdad3bebc4510b9e56891fbf2
carlosbl091998-debug <carlos.bl091998@gmail.com>
2026-08-31 22:58:22 -0600

    Stub AndroidX InitializationProvider in primary dex

--- tools/patch_download_file_provider.sh
+++ tools/patch_download_file_provider.sh
@@ -10,7 +10,7 @@ KEYSTORE="$WORK/test.jks"
 
 for f in "$APK" "$SMALI_JAR" "$BAKSMALI_JAR" "$KEYSTORE"; do test -s "$f"; done
 rm -rf "$PATCH"
-mkdir -p "$PATCH/smali/com/mobile/provider"
+mkdir -p "$PATCH/smali/com/mobile/provider" "$PATCH/smali/androidx/startup"
 unzip -p "$APK" classes.dex > "$PATCH/classes.dex"
 java -jar "$BAKSMALI_JAR" disassemble "$PATCH/classes.dex" -o "$PATCH/smali"
 
@@ -26,6 +26,54 @@ cat > "$PATCH/smali/com/mobile/provider/DownloadFileProvider.smali" <<'EOF'
 .end method
 EOF
 
+cat > "$PATCH/smali/androidx/startup/InitializationProvider.smali" <<'EOF'
+.class public Landroidx/startup/InitializationProvider;
+.super Landroid/content/ContentProvider;
+.source "InitializationProvider.java"
+
+.method public constructor <init>()V
+    .locals 0
+    invoke-direct {p0}, Landroid/content/ContentProvider;-><init>()V
+    return-void
+.end method
+
+.method public onCreate()Z
+    .locals 1
+    const/4 v0, 0x1
+    return v0
+.end method
+
+.method public getType(Landroid/net/Uri;)Ljava/lang/String;
+    .locals 1
+    const/4 v0, 0x0
+    return-object v0
+.end method
+
+.method public insert(Landroid/net/Uri;Landroid/content/ContentValues;)Landroid/net/Uri;
+    .locals 1
+    const/4 v0, 0x0
+    return-object v0
+.end method
+
+.method public query(Landroid/net/Uri;[Ljava/lang/String;Ljava/lang/String;[Ljava/lang/String;Ljava/lang/String;)Landroid/database/Cursor;
+    .locals 1
+    const/4 v0, 0x0
+    return-object v0
+.end method
+
+.method public delete(Landroid/net/Uri;Ljava/lang/String;[Ljava/lang/String;)I
+    .locals 1
+    const/4 v0, 0x0
+    return v0
+.end method
+
+.method public update(Landroid/net/Uri;Landroid/content/ContentValues;Ljava/lang/String;[Ljava/lang/String;)I
+    .locals 1
+    const/4 v0, 0x0
+    return v0
+.end method
+EOF
+
 java -jar "$SMALI_JAR" assemble "$PATCH/smali" -o "$PATCH/classes.dex.new"
 cp "$APK" "$PATCH/unsigned.apk"
 zip -q -d "$PATCH/unsigned.apk" 'META-INF/*.RSA' 'META-INF/*.DSA' 'META-INF/*.EC' 'META-INF/*.SF' 'META-INF/MANIFEST.MF' classes.dex >/dev/null 2>&1 || true
@@ -35,5 +83,5 @@ zipalign -f 4 "$PATCH/unsigned.apk" "$PATCH/aligned.apk"
 apksigner sign --ks "$KEYSTORE" --ks-pass pass:android --key-pass pass:android --ks-key-alias androiddebugkey --out "$PATCH/signed.apk" "$PATCH/aligned.apk"
 apksigner verify --verbose "$PATCH/signed.apk" >/dev/null
 cp "$PATCH/signed.apk" "$APK"
-echo 'DOWNLOAD_FILE_PROVIDER_PRIMARY_DEX_STUB_OK'
+echo 'DOWNLOAD_FILE_AND_INITIALIZATION_PROVIDER_PRIMARY_DEX_STUBS_OK'
 sha256sum "$APK"
